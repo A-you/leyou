d8cb6ae8939f14049412b5f8711f7d1cf38258e7
尤名宇 <582838918@qq.com>
2020-08-08 15:42:12 +0800

    上传文件到本地

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -11,7 +11,8 @@ import lombok.NoArgsConstructor;
 public enum  ExceptionEnum { //
     PRICE_NOT_NULL(400,"价格不能为空"),
     NOT_CATEGORY(404,"查询参数错误"),
-    CREATE_FAIL(400,"添加创建失败")
+    CREATE_FAIL(400,"添加创建失败"),
+    NOT_FILE_TYPES(400,"文件类型不匹配")
     //等价于 public static final PRICE_NOT_NULL=new (400,"价格不能为空")
     ;
     private int code;
--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,14 +1,48 @@
 package com.leyou.upload.service;
 
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Service
-public class UploadService {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
+@Service
+@Slf4j
+public class UploadService {
+    private static  final List<String> ALLOW_TYPES = Arrays.asList("image/jpeg","image/jpg","image/png","image/bmp");
+
+    public static String handleFileName(String fileName){
+        String postFix  = fileName.substring(fileName.lastIndexOf("."));
+        String newName = UUID.randomUUID().toString().replace("-","")+postFix;
+        return newName;
+    }
 
     public String saveImage(MultipartFile file, String name) {
+        System.out.println("进入了");
 
+        if(!ALLOW_TYPES.contains(file.getContentType())){
+            throw new LyException(ExceptionEnum.NOT_FILE_TYPES);
+        }
+        try {
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if(image==null){
+                throw new LyException(ExceptionEnum.NOT_FILE_TYPES);
+            }
+            String newName = handleFileName(file.getOriginalFilename());
+            File dest = new File("/Users/admin/Desktop/java_code/upload",newName );
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("文件上传失败",e);
+        }
         return null;
     }
 }
